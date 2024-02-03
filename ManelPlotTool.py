02bc45f7b4bf2ed40e157ee1bd259221e3f79081
EMANOEL VINICIUS MENEZES DE LIMA <84748132+vinilimaa@users.noreply.github.com>
2024-02-03 17:27:19 -0300

    Update ManelPlotTool.py

--- ManelPlotTool.py
+++ ManelPlotTool.py
@@ -1,18 +1,16 @@
+## Desenvolvido por Emanoel Vinícius 
+## Emanoelufpe@gmail.com
+
+
 import tkinter as tk
 from tkinter import filedialog
 import win32com.client
 import pythoncom
-import time
-
-# Funções de apoio para bounding e SPOINT
+import os
 
 def bounding(Lista):
-    x = []
-    y = []
-
-    for i in range(0, len(Lista), 2):
-        x.append(Lista[i])
-        y.append(Lista[i + 1])
+    x = Lista[::2]
+    y = Lista[1::2]
 
     xmin = min(x)
     ymin = min(y)
@@ -26,44 +24,55 @@ def SPOINT(x, y):
     return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, (x, y))
 
 def plotear_a4(ruta):
-    # Inicia o AutoCAD
-    acad = win32com.client.Dispatch("AutoCAD.Application")
+    try:
+        acad = win32com.client.GetActiveObject("AutoCAD.Application")
+    except:
+        print("Erro: AutoCAD não está ativo.")
+        return
+
     doc = acad.ActiveDocument
     
-    # Obtém o nome do documento e remove a extensão
     nombre = doc.Name
-    nom = nombre[0:len(nombre) - 4]
+    nom = nombre[:-4]
     
     inicio = 1
    
-    # Coloque a pasta onde deseja salvar os arquivos PDF
     try:
         doc.SelectionSets.Item("SS1").Delete()
     except:
-        pass  # Trata a exceção se a seleção não existir ou não puder ser deletada
+        pass
     
     ssget1 = doc.SelectionSets.Add("SS1")
     ssget1.SelectOnScreen()
     
+    total_selecionado = len(ssget1)
+    formato_numero = "{:02d}-{:02d}"
+
     k = 0
     z = inicio - 1
     
-    for entity in ssget1:  # Percorre todas as entidades selecionadas
-        time.sleep(0.05)  # Aguarda para evitar erros de velocidade
+    for i, entity in enumerate(ssget1):
         name = entity.EntityName
         
-        if name == 'AcDbPolyline':  # Verifica se a entidade é uma polilinha
+        if name == 'AcDbPolyline':
             k += 1
-            plotfile = ruta + "\\" + nom + "-0" + str(k + z).strip() + ".pdf"
+            numero_arquivo = formato_numero.format(k + z, total_selecionado)
+            plotfile = os.path.join(ruta, f"{nom}-{numero_arquivo}.pdf")
             enti_pl1 = entity
             m = entity.Coordinates
             n = bounding(m)
             P1 = SPOINT(n[0], n[1])
             P2 = SPOINT(n[2], n[3])
 
-            # Configuração do layout para plotagem
+            largura = abs(n[2] - n[0])
+            altura = abs(n[3] - n[1])
+
             doc.ActiveLayout.ConfigName = "DWG To PDF.pc3"
-            doc.ActiveLayout.CanonicalMediaName = "ISO_full_bleed_A0_(841.00_x_1189.00_MM)"
+            if largura > altura:
+                doc.ActiveLayout.CanonicalMediaName = "ISO_full_bleed_A0_(841.00_x_1189.00_MM)"
+            else:
+                doc.ActiveLayout.CanonicalMediaName = "ISO_full_bleed_A1_(841.00_x_594.00_MM)"
+            
             doc.ActiveLayout.SetWindowToPlot(P1, P2)
             doc.ActiveLayout.PaperUnits = 1
             doc.ActiveLayout.CenterPlot = True
@@ -77,8 +86,8 @@ def plotear_a4(ruta):
             doc.ActiveLayout.StyleSheet = "CTB_ARA.ctb"
             doc.ActiveLayout.PlotType = 4
             doc.ActiveLayout.StandardScale = 0
+            doc.ActiveLayout.PlotWithLineweights = False
             doc.Plot.PlotToFile(plotfile)
-            time.sleep(5)
 
 def browse_folder():
     folder_path = filedialog.askdirectory()
@@ -92,47 +101,55 @@ def start_plotting():
     else:
         status_var.set("Por favor, selecione uma pasta.")
 
-# Cria a janela principal
 root = tk.Tk()
 root.title("Manel Plot Tool")
+root.configure(bg='#2C2F33')  # Cor de fundo cinza escuro
 
-# Variável para armazenar o caminho da pasta
+
+# Centralizar a janela
+window_width = 700
+window_height = 150
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_cordinate = int((screen_width/2) - (window_width/2))
+y_cordinate = int((screen_height/2) - (window_height/2))
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
 folder_var = tk.StringVar()
-
-# Variável para exibir o status da plotagem
 status_var = tk.StringVar()
 status_var.set("Aguardando início da plotagem...")
 
-# Criação dos widgets
-label_folder = tk.Label(root, text="Selecione a pasta de arquivos:")
-entry_folder = tk.Entry(root, textvariable=folder_var, width=50)
-button_browse = tk.Button(root, text="Navegar", command=browse_folder)
-button_start = tk.Button(root, text="Iniciar Plotagem", command=start_plotting)
-label_status = tk.Label(root, textvariable=status_var)
+# Estilos para os widgets
+button_style = {
+    'bg': '#3700B3',  # Cor vermelha para o botão
+    'fg': '#FFFFFF',  # Texto em branco
+    'relief': 'flat'  # Botão sem relevo
+}
 
-# Posicionamento dos widgets na janela
+label_style = {
+    'font': ('Noto Sans', 12),
+    'bg': '#2C2F33',  # Cor de fundo cinza escuro
+    'fg': '#FFFFFF'   # Texto em branco
+}
+
+entry_style = {
+    'font': ('Noto Sans', 10),
+    'bg': '#FFFFFF',  # Cor de fundo branca para o Entry
+    'fg': '#000000'   # Texto em preto
+}
+
+# Criar widgets
+label_folder = tk.Label(root, text="Selecione a pasta de arquivos:", **label_style)
+entry_folder = tk.Entry(root, textvariable=folder_var, width=50, **entry_style)
+button_browse = tk.Button(root, text="Navegar", command=browse_folder, **button_style)
+button_start = tk.Button(root, text="Iniciar Plotagem", command=start_plotting, **button_style)
+label_status = tk.Label(root, textvariable=status_var, **label_style)
+
+# Posicionamento dos widgets
 label_folder.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 entry_folder.grid(row=0, column=1, padx=5, pady=5)
 button_browse.grid(row=0, column=2, padx=5, pady=5)
 button_start.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
 label_status.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
 
-# Criação dos widgets
-label_folder = tk.Label(root, text="Selecione a pasta de arquivos:")
-entry_folder = tk.Entry(root, textvariable=folder_var, width=50)
-button_browse = tk.Button(root, text="Navegar", command=browse_folder)
-button_start = tk.Button(root, text="Iniciar Plotagem", command=start_plotting)
-label_status = tk.Label(root, textvariable=status_var)
-label_about = tk.Label(root, text="Powered by: Emanoel Vinícius", font=("Arial", 10, "italic"), fg="gray")
-
-# Posicionamento dos widgets na janela
-label_folder.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-entry_folder.grid(row=0, column=1, padx=5, pady=5)
-button_browse.grid(row=0, column=2, padx=5, pady=5)
-button_start.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
-label_status.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
-label_about.grid(row=3, column=0, columnspan=3, padx=5, pady=5, sticky="e")
-
-
-# Loop principal da aplicação
 root.mainloop()
